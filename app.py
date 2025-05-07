e05a3e37a29f81344aab47ba15aac8289bdb3d16
Martha-KW <spielsachentests@gmail.com>
2025-05-07 22:51:41 +0200

    add user implemented, add movie implemented with basic features. Needs debugging

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+
 from data_manager.sqlite_data_manager import SQLiteDataManager
 
 app = Flask(__name__)
@@ -11,8 +12,39 @@ def index():
 
 @app.route("/user/<int:user_id>")
 def user_movies(user_id):
+    user = data_manager.get_user_by_id(user_id)
     movies = data_manager.get_user_movies(user_id)
     return render_template("movie_list.html", user_id=user_id, movies=movies)
+
+
+@app.route("/add_user", methods=["GET", "POST"])
+def add_user():
+    if request.method == "POST":
+        username = request.form["username"]
+        data_manager.add_user(username)
+        return redirect("/")  # Zurück zur User-Auswahl
+    return render_template("user_form.html")
+
+
+@app.route('/add_movie/<int:user_id>', methods=['GET', 'POST'])
+def add_movie(user_id):
+    if request.method == 'POST':
+        title = request.form['title']
+        director = request.form['director']
+        year = int(request.form['year'])
+        rating = float(request.form['rating'])
+        genre = request.form['genre']
+        plot = request.form['plot']
+        comment = request.form['comment']
+
+        data_manager.add_movie(title, director, year, rating, user_id,
+                               genre=genre, plot=plot, comment=comment)
+
+        return redirect(url_for('user_movies', user_id=user_id))
+
+    return render_template('add_movie.html', user_id=user_id)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- data_manager/sqlite_data_manager.py
+++ data_manager/sqlite_data_manager.py
@@ -1,41 +1,63 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from data_manager.data_manager_interface import DataManagerInterface
-from models import Base, User, Movie  # falls nicht im Root, Pfad anpassen
+from models import Base, User, Movie
 
 class SQLiteDataManager(DataManagerInterface):
     def __init__(self, db_url="sqlite:///data/movies.db"):
         self.engine = create_engine(db_url)
-        Base.metadata.create_all(self.engine)  # stellt sicher, dass Tabellen da sind
-        self.Session = sessionmaker(bind=self.engine)
+        Base.metadata.create_all(self.engine)
+        SessionLocal = sessionmaker(bind=self.engine)
+        self.Session = sessionmaker(bind=self.engine)  # Session-Instanz
 
     def get_all_users(self):
-        session = self.Session()
-        users = session.query(User).all()
-        session.close()
-        return users
+        return self.Session.query(User).all()
 
     def get_user_movies(self, user_id):
+        user = self.Session.query(User).filter_by(id=user_id).first()
+        if user:
+            return user.movies
+        return []
+
+    def get_user_by_id(self, user_id):
+        return self.Session.query(User).filter_by(id=user_id).first()
+
+    def add_user(self, username):
+        try:
+            new_user = User(username=username)
+            self.Session.add(new_user)
+            self.Session.commit()
+            return new_user.id
+        except Exception as e:
+            self.Session.rollback()
+            print("Error adding user:", e)
+            return None
+
+    def add_movie(self, title, director, year, rating, user_id, genre=None, plot=None,
+                  comment=None):
         session = self.Session()
         try:
-            user = session.query(User).filter_by(id=user_id).first()
-            if user:
-                return user.movies  # gibt Liste von Movie-Objekten zurück
-            return []
+            new_movie = Movie(
+                name=title,  # wichtig: name statt title
+                director=director,
+                year=year,
+                rating=rating,
+                user_id=user_id,
+                genre=genre,
+                plot=plot,
+                comment=comment
+            )
+            session.add(new_movie)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            print("Error adding movie:", e)
         finally:
             session.close()
 
     def update_user_movie(self, movie_id, updated_data):
-        session = self.Session()
-        movie = session.query(Movie).filter_by(id=movie_id).first()
+        movie = self.Session.query(Movie).filter_by(id=movie_id).first()
         if movie:
             for key, value in updated_data.items():
                 setattr(movie, key, value)
-            session.commit()
-        session.close()
-
-if __name__ == "__main__":
-    manager = SQLiteDataManager()
-    movies = manager.get_user_movies(1)
-    for movie in movies:
-        print(movie.name, movie.director)
+            self.Session.commit()
