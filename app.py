fd58dbdaa297bac5366c3e289264efd21d393451
Martha-KW <spielsachentests@gmail.com>
2025-05-08 17:45:13 +0200

    layout and desing optimized, flash messages for updating

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, flash, render_template, redirect, url_for, request
-
 from data_manager.sqlite_data_manager import SQLiteDataManager
+from dotenv import load_dotenv
+import os
+
+
 
 app = Flask(__name__)
+
 data_manager = SQLiteDataManager()
+
+load_dotenv()  # Lädt die .env-Datei
+app.secret_key = os.getenv('SECRET_KEY')
+if not app.secret_key:
+    app.secret_key = 'fallback-key-für-development'
 
 @app.route("/")
 def index():
@@ -63,20 +72,19 @@ def update_movie(user_id, movie_id):
 
         success = data_manager.update_user_movie(movie_id, updated_data)
         if not success:
-            flash("Movie not found!", "error")
+            flash("Movie not found!", "error")  # Roter Alert
         else:
-            flash("Movie updated successfully!", "success")
+            flash("Movie updated successfully!", "success")  # Grüner Alert
 
         return redirect(url_for('user_movies', user_id=user_id))
 
     # GET-Request: Formular mit aktuellen Daten anzeigen
     movie = data_manager.get_movie_by_id(movie_id)
     if not movie:
-        flash("Movie not found!", "error")
+        flash("Movie not found!", "error")  # Roter Alert
         return redirect(url_for('user_movies', user_id=user_id))
 
     return render_template('edit_movie.html', user_id=user_id, movie=movie)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
